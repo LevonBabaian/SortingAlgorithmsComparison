5dd6db7aeb8df13f4016191f3cc438a4199c9bf5
LevonBabaian <146972323+LevonBabaian@users.noreply.github.com>
2024-06-12 00:26:06 +0400

    Add files via upload

---
+++ SortingAlgorithmComparison/QuickSortTemplate.cs
@@ -0,0 +1,12 @@
+﻿// Template for Quick Sort
+public class QuickSortTemplate : SortTemplate
+{
+    public QuickSortTemplate(ISortAlgorithm algorithm) : base(algorithm)
+    {
+    }
+
+    public override void Sort(int[] array)
+    {
+        SortTemplateMethod(array);
+    }
+}
